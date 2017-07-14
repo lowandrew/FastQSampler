88295a9da6b57256f1bba6c1dab386508d4a6fd7
Andrew Low <andrew.low@inspection.gc.ca>
2017-07-14 08:34:05 -0400

    Fixed installation issue.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "FastQSampler",
-	version = "0.1.0",
+	version = "0.1.1",
 	scripts = ["sampler.py","sampler_wrapper.py"],
 	install_requires=["biopython"],
 	author="Andrew Low",
